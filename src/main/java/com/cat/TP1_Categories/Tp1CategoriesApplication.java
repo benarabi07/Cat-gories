90f47d22327fc670fb337312f117bb2d4a4e9df5
MAHAMAT Abbakabir <abbakabirarabi@gmail.com>
2024-11-19 16:32:02 +0100

    initialisation du projet

---
+++ src/main/java/com/cat/TP1_Categories/Tp1CategoriesApplication.java
@@ -0,0 +1,13 @@
+package com.cat.TP1_Categories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1CategoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1CategoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cat/TP1_Categories/Tp1CategoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cat.TP1_Categories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1CategoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
